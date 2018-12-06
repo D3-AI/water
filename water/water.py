9aa451bd8b2fa1638721684f358726b67dbcb68c
Manuel Alvarez <manuel@pythiac.com>
2018-12-06 18:32:22 +0100

    Make methods and attributes private, add loading of templates from db

--- water/water.py
+++ water/water.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
+
+import json
 import logging
-import warnings
+import os
 from collections import defaultdict
 
 import numpy as np
@@ -8,63 +10,81 @@ from btb import HyperParameter
 from btb.tuning import GP
 from mlblocks import MLPipeline
 from sklearn.exceptions import NotFittedError
+from sklearn.metrics import accuracy_score, r2_score
 from sklearn.model_selection import KFold, StratifiedKFold
-
-warnings.filterwarnings('ignore', category=DeprecationWarning)
 
 LOGGER = logging.getLogger(__name__)
 
 
 class TimeSeriesEstimator(object):
-    template = {}
-    maximize_score = None
-    cv_class = None
 
-    def __init__(self, scorer, cv=None, cv_splits=None, random_state=0,
-                 template=None, hyperparameters=None, *args, **kwargs):
+    template = None
+    fitted = False
 
-        if not (cv or cv_splits):
-            raise ValueError('Either a CV object or a cv_split must be provided.')
+    _cv_class = None
+    _score = None
+    _cost = None
+    _tuner = None
+    _pipeline = None
 
-        if cv and not callable(getattr(cv, 'split', None)):
-            raise ValueError('The cv object must have an `split` method')
+    def _get_cv(self, cv_splits, random_state):
+        return self._cv_class(n_splits=cv_splits, shuffle=True, random_state=random_state)
 
-        self.cv = cv or self.cv_class(n_splits=cv_splits, shuffle=True, random_state=random_state)
-        self.random_state = random_state
-        self.scorer = scorer
-        self.fitted = False
-        self.tuner = None
+    def _load_mlpipeline(self, template):
+        if not isinstance(template, dict):
+            template_name = template
+            if os.path.isfile(template_name):
+                with open(template_name, 'r') as template_file:
+                    template = json.load(template_file)
 
-        template = template or self.template
-        self.pipeline = MLPipeline.from_dict(template)
+            elif self._db:
+                template = self._db.load_template(template_name)
+
+            if not template:
+                raise ValueError('Unknown template {}'.format(template_name))
+
+            self.template = template
+
+        return MLPipeline.from_dict(template)
+
+    def __init__(self, db=None, template=None, hyperparameters=None,
+                 scorer=None, cost=False, cv=None, cv_splits=5, random_state=0):
+
+        self._cv = cv or self._get_cv(cv_splits, random_state)
+
+        if scorer:
+            self._score = scorer
+
+        self._cost = cost
+
+        self._db = db
+
+        self._pipeline = self._load_mlpipeline(template or self.template)
 
         if hyperparameters:
-            self.set_hyperparameters(hyperparameters)
+            self._pipeline.set_hyperparameters(hyperparameters)
 
     def get_hyperparameters(self):
-        return self.pipeline.get_hyperparameters()
+        return self._pipeline.get_hyperparameters()
 
     def set_hyperparameters(self, hyperparameters):
-        self.pipeline.set_hyperparameters(hyperparameters)
+        self._pipeline.set_hyperparameters(hyperparameters)
         self.fitted = False
 
-    def get_tunable_hyperparameters(self):
-        return self.pipeline.get_tunable_hyperparameters()
-
     @staticmethod
-    def clone_pipeline(pipeline):
+    def _clone_pipeline(pipeline):
         return MLPipeline.from_dict(pipeline.to_dict())
 
-    def _is_better(self, score, best_score):
-        if self.maximize_score:
-            return score > best_score
+    def _is_better(self, score):
+        if self._cost:
+            return score < self._best_score
 
-        return score < best_score
+        return score > self._best_score
 
-    def get_tunables(self):
+    def _get_tunables(self):
         tunables = []
         tunable_keys = []
-        for block_name, params in self.pipeline.get_tunable_hyperparameters().items():
+        for block_name, params in self._pipeline.get_tunable_hyperparameters().items():
             for param_name, param_details in params.items():
                 key = (block_name, param_name)
                 param_type = param_details['type']
@@ -81,25 +101,26 @@ class TimeSeriesEstimator(object):
 
         return tunables, tunable_keys
 
-    def score_pipeline(self, pipeline, X, y, data, time_index, index):
+    def _score_pipeline(self, pipeline, X, y, data):
         scores = []
 
-        for train_index, test_index in self.cv.split(X, y):
+        for fold, (train_index, test_index) in enumerate(self._cv.split(X, y)):
+            LOGGER.debug('Scoring fold %s', fold)
             X_train, X_test = X.iloc[train_index], X.iloc[test_index]
             y_train, y_test = y.iloc[train_index], y.iloc[test_index]
 
-            fit_args = self.get_pipeline_args(X_train, y_train, data, time_index, index)
-            pipeline = self.clone_pipeline(pipeline)
-            pipeline.fit(**fit_args)
+            pipeline = self._clone_pipeline(pipeline)
+            pipeline.fit(X_train, y_train, **data)
 
-            predict_args = self.get_pipeline_args(X_test, None, data, time_index, index)
-            predictions = pipeline.predict(**predict_args)
-            score = self.scorer(y_test, predictions)
+            predictions = pipeline.predict(X_test, **data)
+            score = self._score(y_test, predictions)
+
+            LOGGER.debug('Fold fold %s score: %s', fold, score)
             scores.append(score)
 
         return np.mean(scores)
 
-    def to_dicts(self, hyperparameters):
+    def _to_dicts(self, hyperparameters):
 
         params_tree = defaultdict(dict)
         for (block, hyperparameter), value in hyperparameters.items():
@@ -119,7 +140,7 @@ class TimeSeriesEstimator(object):
 
         return params_tree
 
-    def to_tuples(self, params_tree, tunable_keys):
+    def _to_tuples(self, params_tree, tunable_keys):
         param_tuples = defaultdict(dict)
         for block_name, params in params_tree.items():
             for param, value in params.items():
@@ -129,94 +150,53 @@ class TimeSeriesEstimator(object):
 
         return param_tuples
 
-    @staticmethod
-    def get_pipeline_args(X, y, data, time_index, index):
-        """Build a dictionary with all the arguments needed to call the pipeline.
+    def _get_tuner(self):
+        tunables, tunable_keys = self._get_tunables()
+        tuner = GP(tunables)
 
-        Args:
-            X(pd.DataFrame):
-            y:
-            data:
+        # Inform the tuner about the score that the default hyperparmeters obtained
+        param_tuples = self._to_tuples(self._pipeline.get_hyperparameters(), tunable_keys)
+        tuner.add(param_tuples, self._best_score)
 
-        """
+        return tuner
 
-        entities = {
-            'main': (X, index, None),
-            'timeseries': (data, time_index, 'time')
-        }
-
-        relationships = [('main', index, 'timeseries', index)]
-
-        pipeline_args = {
-            'X': X,
-            'entities': entities,
-            'relationships': relationships,
-            'target_entity': 'main'
-        }
-        if y is not None:
-            pipeline_args['y'] = y
-
-        return pipeline_args
-
-    def tune(self, X, y, data, time_index, index, iterations=10):
-
-        # Build the tuner passing the tunables for
-        # this pipeline
-        if not self.tuner:
-            tunables, tunable_keys = self.get_tunables()
-            self.tuner = GP(tunables)
-
-            # Compute an initial best score
-            best_score = self.score_pipeline(self.pipeline, X, y, data, time_index, index)
-
-            LOGGER.info("Default hyperparameters score: {}".format(best_score))
-
-            # Inform the tuner about the score that the default hyperparmeters obtained
-            param_tuples = self.to_tuples(self.pipeline.get_hyperparameters(), tunable_keys)
-            self.tuner.add(param_tuples, best_score)
+    def tune(self, X, y, data, iterations=10):
+        if not self._tuner:
+            LOGGER.info('Scoring the default pipeline')
+            self._best_score = self._score_pipeline(self._pipeline, X, y, data)
+            self._tuner = self._get_tuner()
 
         for i in range(iterations):
-            LOGGER.info("Scoring pipeline {}".format(i + 1))
+            LOGGER.info('Scoring pipeline %s', i + 1)
 
-            # Get hyperparameters proposal
-            params = self.tuner.propose(1)
+            params = self._tuner.propose(1)
+            param_dicts = self._to_dicts(params)
 
-            # Convert hyperparameters to tree of dicts as expected by the MLPipeline
-            param_dicts = self.to_dicts(params)
-
-            # Get a new instance of the pipeline with the same primitives
-            candidate = self.clone_pipeline(self.pipeline)
-
-            # Set the new hyperparameters to the cloned pipeline and calculate their score
+            candidate = self._clone_pipeline(self._pipeline)
             candidate.set_hyperparameters(param_dicts)
 
-            score = self.score_pipeline(candidate, X, y, data, time_index, index)
+            score = self._score_pipeline(candidate, X, y, data)
+            LOGGER.info('Pipeline %s score: %s', i + 1, score)
 
-            # Inform the tuner about the obtained score
-            self.tuner.add(params, score)
+            self._tuner.add(params, score)
 
-            # See if this pipeline is the best one so far
-            if self._is_better(score, best_score):
-                best_score = score
+            if self._is_better(score):
+                self._best_score = score
                 self.set_hyperparameters(param_dicts)
 
-    def fit(self, X, y, data, time_index, index):
-        pipeline_args = self.get_pipeline_args(X, y, data, time_index, index)
-        self.pipeline.fit(**pipeline_args)
+    def fit(self, X, y, data):
+        self._pipeline.fit(X, y, **data)
         self.fitted = True
 
-    def predict(self, X, data, time_index, index):
+    def predict(self, X, data):
         if not self.fitted:
             raise NotFittedError()
 
-        pipeline_args = self.get_pipeline_args(X, None, data, time_index, index)
-
-        return self.pipeline.predict(**pipeline_args)
+        return self._pipeline.predict(X, **data)
 
 
 class TimeSeriesClassifier(TimeSeriesEstimator):
-    maximize_score = True
-    cv_class = StratifiedKFold
+    _cv_class = StratifiedKFold
     template = {
         'primitives': [
             'mlprimitives.preprocessing.ClassEncoder',
@@ -241,10 +221,12 @@ class TimeSeriesClassifier(TimeSeriesEstimator):
         }
     }
 
+    def _score(self, y_true, y_pred):
+        return accuracy_score(y_true, y_pred)
+
 
 class TimeSeriesRegressor(TimeSeriesEstimator):
-    maximize_score = False
-    cv_class = KFold
+    _cv_class = KFold
     template = {
         'primitives': [
             'featuretools.dfs',
@@ -268,3 +250,6 @@ class TimeSeriesRegressor(TimeSeriesEstimator):
             }
         }
     }
+
+    def _score(self, y_true, y_pred):
+        return r2_score(y_true, y_pred)
